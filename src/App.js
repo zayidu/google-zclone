c022e64604c579ab9990aa418683e0876b2d635e
Zayidu Ansari <zayidu11@gmail.com>
2020-09-12 16:54:05 +0400

    Project Initialized and Added Header

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+import Home from './Pages/Home';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route path="/search">{/* <SearchPage/> */}</Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
