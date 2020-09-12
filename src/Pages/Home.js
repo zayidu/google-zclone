567fc6e0f6ef277699e673c74186ec78a3b188bc
Zayidu Ansari <zayidu11@gmail.com>
2020-09-12 19:04:42 +0400

    :Moving to the search page:

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -3,6 +3,7 @@ import './home.css';
 import { Link } from 'react-router-dom';
 import AppsIcon from '@material-ui/icons/Apps';
 import { Avatar } from '@material-ui/core';
+import Search from './../Components/Search';
 
 function Home() {
   return (
@@ -22,7 +23,13 @@ function Home() {
           />
         </div>
       </div>
-      <div className="home__body"></div>
+      <div className="home__body">
+        <img
+          src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png"
+          alt=""
+        />
+        <Search />
+      </div>
     </div>
   );
 }
