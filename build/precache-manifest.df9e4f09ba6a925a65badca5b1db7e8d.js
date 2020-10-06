23b48f8f691570c4930eb5224fdc67a436e970ce
Zayidu Ansari <zayidu11@gmail.com>
2020-10-06 22:48:55 +0400

    google-zclone Complete_v2

---
+++ build/precache-manifest.df9e4f09ba6a925a65badca5b1db7e8d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63ed61aa6a40ad730bcd652760de3dc3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1dedf556070db501daa1",
+    "url": "/static/css/main.6dabf0ca.chunk.css"
+  },
+  {
+    "revision": "2caad267494881e30b39",
+    "url": "/static/js/2.2bdd9518.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.2bdd9518.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1dedf556070db501daa1",
+    "url": "/static/js/main.af6d4d59.chunk.js"
+  },
+  {
+    "revision": "f9369348e1d752d65e70",
+    "url": "/static/js/runtime-main.da8897b0.js"
+  }
+]);
